a1f3fa6bfe57adc69e3f6c673b9773a9480281fc
usrmrz <usrmrz@gmail.com>
2024-12-09 19:06:55 +0200

    second commit

---
+++ app/src/main/java/dev/usrmrz/githubrepolist/ui/activities/MainActivity.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2018 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package dev.usrmrz.githubrepolist.ui.activities
+
+import android.app.Activity
+import android.os.Bundle
+import dev.usrmrz.githubrepolist.R
+
+//import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : Activity() {
+
+  private val items = listOf(
+      "JetBrains/kotlin - The Kotlin Programming Language",
+      "exercism/kotlin - Exercism exercises in Kotlin",
+      "cbeust/kobalt - A Kotlin-based build system for the JVM",
+      "JetBrains/kotlin - The Kotlin Programming Language",
+      "exercism/kotlin - Exercism exercises in Kotlin",
+      "cbeust/kobalt - A Kotlin-based build system for the JVM",
+      "JetBrains/kotlin - The Kotlin Programming Language"
+  )
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+//    repoList.layoutManager = LinearLayoutManager(this)
+//    repoList.adapter = RepoListAdapter(items)
+
+    val url = "https://api.github.com/search/repositories?q=mario+language:kotlin&sort=stars&order=desc"
+  }
+}
